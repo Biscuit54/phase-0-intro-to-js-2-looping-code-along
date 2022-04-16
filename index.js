6c8f256c2050b6b326fde7dad32e87a6419a46da
Jacob Delgadillo <jacobdelgadillo751@gmail.com>
2022-04-15 23:26:55 -0700

    commit tree fbd8289ddede7ce00f250636f1b9137fbc67fc6b

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const messages = [];
+    for (let i = 0; i < names.length; i++){
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+console.log(writeCards(['Guadalupe', 'Ollie', 'Aki'], ' birthday'));
+function countDown(){
+    let i = 11;
+    while (i>0)
+        console.log(i--);
+        
+        let countDown = 4;
+while (countDown > 0 ) {
+  console.log(countDown--);
+}
+
+    }
+
